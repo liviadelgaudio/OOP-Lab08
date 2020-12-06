3af34e57dce76ea6707eedb04438b4f66b829b38
liviadelgaudio <livia.delgaudio@studio.unibo.it>
2020-12-06 10:26:17 +0100

    Improvements

--- src/it/unibo/oop/lab/simplegui/MiniGUI.java
+++ src/it/unibo/oop/lab/simplegui/MiniGUI.java
@@ -13,7 +13,6 @@ import java.util.Random;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
